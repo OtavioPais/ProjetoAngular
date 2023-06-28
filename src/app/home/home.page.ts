f702cce84c269b8c575fae2cc4c32c6c5bf08ce8
OtavioPais <135171904+OtavioPais@users.noreply.github.com>
2023-06-28 20:35:59 +0000

    Calculadora Iphone

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,24 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
+
 export class HomePage {
-  constructor() {}
+  result: string = ''; // Resultado final
+
+  AddValor(item: string){
+    this.result += item //Pega a sequencia e atribui o novo item
+  }
+
+  LimpaResultado(){
+    this.result = '' //Limpa resultado, atribuindo uma string vazia
+  }
+  LimpaValor(){
+    this.result = this.result.slice(0, -1) //Pega ultima string e remove da variavel
+  }
+  
+  Calcula(){
+    this.result = eval(this.result) // Mostra resultado na tela, utilizando váriavel do tipo String
+  }
 }
